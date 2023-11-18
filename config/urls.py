4a80a0b2fa3ea3de6a286af6a1292366662b74b3
Chesed Kim <starmireu@gmail.com>
2023-11-18 17:35:20 +0900

    build : 초기 구성 및 users simplejwt 구성

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,37 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Project Kanban API",
+        default_version="v1",
+        description="Project Kanban 일정 관리 API 입니다.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(url="https://github.com/mireu-san/project_kanban"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
+    # swagger
+    re_path(
+        "^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(r"^$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-root"),
+    re_path(
+        r"redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc-v1",
+    ),
+    # url
     path("admin/", admin.site.urls),
+    path("api/v1/users/", include("users.urls")),
+    # path("api/v1/teams/", include("teams.urls")),
+    # path("api/v1/kanban/", include("kanban.urls")),
+    # path("api/v1/analyst/", include("analyst.urls")),
 ]
