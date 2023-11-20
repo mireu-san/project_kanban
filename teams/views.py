48dbbee91dffb5dbc526a874e88fe0d63126698d
Jongwan Kim <mireu-san>
2023-11-20 15:47:09 +0900

    feat : 팀원 초대 승락 API 추가 및 초대 거절 추가

--- teams/views.py
+++ teams/views.py
@@ -1,22 +1,21 @@
 from rest_framework import status, views
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.exceptions import PermissionDenied, NotFound
 from .models import Team, TeamInvitation
 from .serializers import TeamSerializer, TeamInvitationSerializer
 from users.models import User
-
 from rest_framework import permissions
-from rest_framework.exceptions import NotFound
 
 
 class TeamCreateAPIView(views.APIView):
     permission_classes = [IsAuthenticated]
 
+    # 팀 생성 API
     def post(self, request):
         serializer = TeamSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
-            # Set the 'created_by' field to the current user
+            # 생성자를 현재 사용자로 설정
             serializer.save(created_by=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -25,96 +24,88 @@ class TeamCreateAPIView(views.APIView):
 class TeamInvitationCreateAPIView(views.APIView):
     permission_classes = [IsAuthenticated]
 
+    # 팀 초대 API
     def post(self, request, team_id):
         try:
             team = Team.objects.get(id=team_id)
-            # Ensure the user making the request is the team leader
+            # 요청자가 팀 리더인지 확인
             if team.created_by != request.user:
-                raise PermissionDenied("You must be the team leader to invite members.")
+                raise PermissionDenied("팀 리더만이 멤버를 초대할 수 있습니다.")
 
-            # Get the user to be invited
+            # 초대할 사용자 가져오기
             invitee_username = request.data.get("username")
             invitee = User.objects.get(username=invitee_username)
-            # Create the invitation
+            # 초대 생성
             invitation = TeamInvitation.objects.create(team=team, invitee=invitee)
             serializer = TeamInvitationSerializer(invitation)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         except Team.DoesNotExist:
-            return Response(
-                {"error": "Team not found."}, status=status.HTTP_404_NOT_FOUND
-            )
+            return Response({"error": "팀을 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
         except User.DoesNotExist:
-            return Response(
-                {"error": "User to invite not found."}, status=status.HTTP_404_NOT_FOUND
-            )
+            return Response({"error": "초대할 사용자를 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-
-class IsTeamLeader(permissions.BasePermission):
-    def has_permission(self, request, view):
-        # `team_id`는 URL로부터 얻어옵니다.
-        team_id = view.kwargs.get("team_id")
-        try:
-            team = Team.objects.get(id=team_id)
-            # 팀 리더인지 확인합니다.
-            return team.created_by == request.user
-        except Team.DoesNotExist:
-            return False
-
-
-class TeamInvitationCreateAPIView(views.APIView):
-    permission_classes = [IsAuthenticated, IsTeamLeader]
-
-    def post(self, request, team_id):
-        # IsTeamLeader 퍼미션에 의해 여기까지 도달했다면,
-        # 요청자는 팀의 리더임이 보장됩니다.
-        team = Team.objects.get(id=team_id)
-
-        # 초대할 사용자를 가져옵니다.
-        invitee_username = request.data.get("username")
-        try:
-            invitee = User.objects.get(username=invitee_username)
-        except User.DoesNotExist:
-            return Response(
-                {"error": "User to invite not found."}, status=status.HTTP_404_NOT_FOUND
-            )
-
-        # 이미 초대된 사용자인지 확인합니다.
-        if TeamInvitation.objects.filter(team=team, invitee=invitee).exists():
-            return Response(
-                {"error": "Invitation already exists."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-        # 초대를 생성합니다.
-        invitation = TeamInvitation.objects.create(team=team, invitee=invitee)
-        serializer = TeamInvitationSerializer(invitation)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class TeamInvitationAcceptAPIView(views.APIView):
     permission_classes = [IsAuthenticated]
 
+    # 초대 승낙 API
     def post(self, request, invitation_id):
         try:
-            invitation = TeamInvitation.objects.get(
-                id=invitation_id, invitee=request.user
-            )
+            invitation = TeamInvitation.objects.get(id=invitation_id, invitee=request.user)
 
-            # 초대가 pending 상태인지 확인합니다.
+            # 초대가 대기 상태인지 확인
             if invitation.status != "pending":
-                return Response(
-                    {"error": "Invitation is already responded."},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
+                return Response({"error": "초대에 이미 응답되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
+            # 초대 상태를 'accepted'로 변경
             invitation.status = "accepted"
             invitation.save()
-            return Response(
-                {"status": "Invitation accepted"}, status=status.HTTP_200_OK
-            )
+            return Response({"status": "초대가 승낙되었습니다."}, status=status.HTTP_200_OK)
 
         except TeamInvitation.DoesNotExist:
-            raise NotFound(detail="Invitation not found.")
+            raise NotFound(detail="초대를 찾을 수 없습니다.")
+
+
+class TeamMemberListView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    # 팀 멤버 목록 조회 API
+    def get(self, request, team_id):
+        try:
+            team = Team.objects.get(id=team_id)
+            # 팀의 수락된 초대 목록 가져오기
+            invitations = TeamInvitation.objects.filter(team=team, status="accepted")
+            members = [invitation.invitee for invitation in invitations]
+            serializer = UserSerializer(members, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Team.DoesNotExist:
+            return Response({"error": "팀을 찾을 수 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+
+
+class TeamInvitationRespondAPIView(views.APIView):
+    permission_classes = [IsAuthenticated]
+
+    # 초대 응답 API (승낙/거절)
+    def post(self, request, invitation_id):
+        response = request.data.get("response", "")  # 'accept' 또는 'decline'
+        try:
+            invitation = TeamInvitation.objects.get(id=invitation_id, invitee=request.user)
+
+            if invitation.status != "pending":
+                return Response({"error": "초대에 이미 응답되었습니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+            if response.lower() == "accept":
+                invitation.status = "accepted"
+            elif response.lower() == "decline":
+                invitation.status = "declined"
+            else:
+                return Response({"error": "잘못된 응답입니다."}, status=status.HTTP_400_BAD_REQUEST)
+
+            invitation.save()
+            return Response({"status": f"초대가 {response}되었습니다."}, status=status.HTTP_200_OK)
+
+        except TeamInvitation.DoesNotExist:
+            raise NotFound(detail="초대를 찾을 수 없습니다.")
